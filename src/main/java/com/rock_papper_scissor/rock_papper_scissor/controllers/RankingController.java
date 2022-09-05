b9fae0b52f16221748856cc1c2a5dc47f2184df0
Yassineelghizi <yassine.ghizi2@gmail.com>
2022-09-05 23:17:08 +0100

    Completed_ranking_sort

--- src/main/java/com/rock_papper_scissor/rock_papper_scissor/controllers/RankingController.java
+++ src/main/java/com/rock_papper_scissor/rock_papper_scissor/controllers/RankingController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -25,42 +24,50 @@ public class RankingController {
 
 
     @GetMapping
-//    public HttpEntity<HashMap<String, Object>> get_ranking(){
-    public String get_ranking() {
-        Optional<User> u = userRepository.findById(1L);
-        if (u.isPresent()) {
-            AtomicReference<Integer> wins = new AtomicReference<>(0);
-            AtomicReference<Integer> draws = new AtomicReference<>(0);
-            AtomicReference<Integer> loses = new AtomicReference<>(0);
+    public List<HashMap<Object, Object>> get_ranking() {
+        List<User> list_users = userRepository.findAll();
+        List<HashMap<Object, Object>> response = new ArrayList<>();
+        list_users.forEach(user -> {
+            response.add(this.get_user_score(user));
+        });
 
-            User uu = u.get();
+        Collections.sort(response, new Comparator<HashMap<Object, Object>>() {
+            @Override
+            public int compare(HashMap<Object, Object> o1, HashMap<Object, Object> o2) {
+                return (-1) * Integer.compare((int) o1.get("score"), (int) o2.get("score"));
+            }
+        });
+        return response;
+    }
 
-            uu.get_wins().forEach(game -> {
-                switch (game.getState()) {
-                    case 1:
-                        wins.updateAndGet(v -> v + 1);
-                        break;
-                    case 0:
-                        draws.updateAndGet(v -> v + 1);
-                        break;
-                    case -1:
-                        loses.updateAndGet(v -> v + 1);
-                    default:
-                        break;
-                }
-            });
+    public HashMap<Object, Object> get_user_score(User user) {
+        AtomicReference<Integer> wins = new AtomicReference<>(0);
+        AtomicReference<Integer> draws = new AtomicReference<>(0);
+        AtomicReference<Integer> loses = new AtomicReference<>(0);
 
-            Integer score = (wins.get() * 3) + draws.get();
-            HashMap<Object, Object> data = new HashMap<>();
-            data.put("wins", wins);
-            data.put("draws", draws);
-            data.put("loses", loses);
-            data.put("score", score);
-            data.put("user_name", uu.getName());
-            data.put("user_img", uu.getImg());
+        user.get_wins().forEach(game -> {
+            switch (game.getState()) {
+                case 1:
+                    wins.updateAndGet(v -> v + 1);
+                    break;
+                case 0:
+                    draws.updateAndGet(v -> v + 1);
+                    break;
+                case -1:
+                    loses.updateAndGet(v -> v + 1);
+                default:
+                    break;
+            }
+        });
 
-            System.out.println(data);
-        }
-        return "OK";
+        Integer score = (wins.get() * 3) + draws.get();
+        HashMap<Object, Object> data = new HashMap<>();
+        data.put("wins", wins);
+        data.put("draws", draws);
+        data.put("loses", loses);
+        data.put("score", score);
+        data.put("user_name", user.getName());
+        data.put("user_img", user.getImg());
+        return data;
     }
 }
